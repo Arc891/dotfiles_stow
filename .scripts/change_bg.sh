4c0784378bba0271c40a9f1f44234074d2468686
Arc891 <ezra.hulsman@gmail.com>
2024-03-06 21:27:16 +0100

    Update change_bg to have random list with no repeats, taking 1 hour to finish (make sure 12 images in Pics/Bgs itself)

--- .scripts/change_bg.sh
+++ .scripts/change_bg.sh
@@ -1,10 +1,11 @@
-bg=$(find ~/Pictures/Backgrounds/ | shuf -n 1)
-
-echo $bg
-
 swww query || swww init
 
-swww img $bg --transition-fps 60 --transition-type wipe --transition-duration 2
+find ~/Pictures/Backgrounds/ -maxdepth 1 -type f -print0 |
+  while IFS= read -r -d '' bg; do 
+    echo $bg;
+    swww img $bg --transition-fps 60 --transition-type wipe --transition-duration 2
+    sleep 300;
+  done;
 
 #ln -sf $bg /usr/share/sddm/themes/sdt/wallpaper.jpg
 
