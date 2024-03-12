ef626a3da135eff5c654411a80249f5b5fd16902
Arc891 <ezra.hulsman@gmail.com>
2024-03-12 15:27:52 +0100

    Made change_bg.sh endless so no cron/sysctl needed

--- .scripts/change_bg.sh
+++ .scripts/change_bg.sh
@@ -1,11 +1,13 @@
 swww query || swww init
 
-find ~/Pictures/Backgrounds/ -maxdepth 1 -type f -print0 |
-  sort --zero-terminated --random-sort |
-  while IFS= read -r -d '' bg; do 
-    echo $bg;
-    swww img $bg --transition-fps 60 --transition-type wipe --transition-duration 2
-    sleep 300;
-  done;
+while true; do
+  find ~/Pictures/Backgrounds/ -maxdepth 1 -type f -print0 |
+    sort --zero-terminated --random-sort |
+    while IFS= read -r -d '' bg; do 
+      echo $bg;
+      swww img $bg --transition-fps 60 --transition-type wipe --transition-duration 2
+      sleep 300;
+    done;
+done;
 
 #ln -sf $bg /usr/share/sddm/themes/sdt/wallpaper.jpg
