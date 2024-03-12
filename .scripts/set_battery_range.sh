5fd0bd39dd31e5fdb5d6c07ca895b1b9fad45541
Arc891 <ezra.hulsman@gmail.com>
2024-03-12 15:26:24 +0100

    Added spotify + album art & moved bat range to battery module & new window outputs

--- .scripts/set_battery_range.sh
+++ .scripts/set_battery_range.sh
@@ -1,3 +1,5 @@
+bat_dir="/sys/class/power_supply/BAT0/"
+echo "Current charge: $(cat $bat_dir/charge_start_threshold) -> $(cat $bat_dir/charge_stop_threshold)";
 read -p "Start charging below: " b;
 read -p "Stop charging above: " a;
 echo $b | sudo tee /sys/class/power_supply/BAT0/charge_start_threshold;
