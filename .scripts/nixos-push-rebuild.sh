b98994b24f903e62d1878c9e9d7449d66c360b60
Arc891 <ezra.hulsman@gmail.com>
2025-03-18 14:35:40 +0100

    Added dry-activate build before committing

--- .scripts/nixos-push-rebuild.sh
+++ .scripts/nixos-push-rebuild.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    echo "You must be root to do this." 1>&2
+    exit 100
+fi
 cd /etc/nixos/
 
 git status
@@ -8,6 +13,9 @@ if [[ $add_all == 'N' ]] || [[ $add_all == 'n' ]]; then
 	exit 1;
 fi
 git add .
+git add *
+
+nixos-rebuild dry-activate
 
 git status
 
@@ -15,5 +23,8 @@ read -p 'Commit message: ' msg;
 git commit -m "$msg";
 git push
 
-read -p 'Option to rebuild: ' rebuild_opt;
+read -p 'Option to rebuild (switch by default): ' rebuild_opt;
+if [[ -z "$rebuild_opt" ]]; then
+	rebuild_opt="switch";
+fi	
 nixos-rebuild $rebuild_opt
